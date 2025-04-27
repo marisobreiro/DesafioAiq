b4a7decb42e7c94b6b76a1f6a28cc2e062901c73
marisobreiro <marianasobreiro93@gmail.com>
2025-04-27 01:01:05 -0300

    feat: integrate React Query for data fetching in App component

--- App.tsx
+++ App.tsx
@@ -1,30 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {createStaticNavigation} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
-import {HomeScreen} from './src/screens/HomeScreen/HomeScreen';
-
-const RootStack = createNativeStackNavigator({
-  initialRouteName: 'Home',
-  screens: {
-    Home: {
-      screen: HomeScreen,
-      options: {
-        headerShown: false,
-      },
-    },
-  },
-});
+import {RootStack} from './src/config/navigation';
 
 const Navigation = createStaticNavigation(RootStack);
 
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <Navigation />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navigation />
+    </QueryClientProvider>
+  );
 }

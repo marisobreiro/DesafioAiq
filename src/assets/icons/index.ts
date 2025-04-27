27f503fa14ef6b404e1d78de50704968ecafb894
marisobreiro <marianasobreiro93@gmail.com>
2025-04-27 01:01:43 -0300

    feat: add new SVG icons (badge-percent, dollar-sign, heart, mail, menu, star) and avatar image

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export {default as BadgeDiscont} from './badge-percent.svg';
+export {default as DollarSign} from './dollar-sign.svg';
+export {default as Heart} from './heart.svg';
+export {default as Menu} from './menu.svg';
+export {default as Star} from './star.svg';
